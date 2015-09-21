e487f15262eea597622defe18ce38dade7c1460c
Azat S <azatklevec@gmail.com>
2015-09-21 12:22:51 +0300

    Add plugin logic

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 'use strict';
 
 var postcss = require('postcss');
-
 module.exports = postcss.plugin('postcss-responsive-images', function (opts) {
-    opts = opts || {};
-
-    return function (css) {
-        // Plugin logic goes here
+  return function (css) {
+    var responsiveImages = function(decl) {
+      var origRule = decl.parent;
+      origRule.append('max-width: 100%; height: auto; display: block;');
+      decl.remove();
     };
+    var centerImages = function(decl) {
+      var origRule = decl.parent;
+      origRule.append('max-width: 100%; height: auto; display: block; margin: 20px auto');
+      decl.remove();
+    }
+    css.walkDecls('image', function(decl) {
+      switch (decl.value) {
+        case 'responsive':
+          responsiveImages(decl);
+          break;
+        case 'responsive-center':
+          centerImages(decl);
+          break;
+      }
+    });
+  };
 });
